5304500bc4984576606293259da14af29100f5c6
Fernando <fer.gab.sua@gmail.com>
2024-10-11 20:35:26 -0300

    mod

--- backend/waki_server/football_api/serializers.py
+++ backend/waki_server/football_api/serializers.py
@@ -7,6 +7,9 @@ class LeagueSerializer(serializers.ModelSerializer):
         fields = ['id_league', 'name', 'logo', 'country']
 
 class MatchSerializer(serializers.ModelSerializer):
+    league = LeagueSerializer(read_only=True) 
+
+    
     class Meta:
         model = Match
         fields = ['id_fixture','league','date','home_team','home_team_logo','away_team','away_team_logo','home_team_goals','away_team_goals','match_status','home_odds','draw_odds','away_odds']
