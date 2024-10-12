5e5fa21132ef1c476af40c4d5ab962f0169fc361
gabyp05 <gabyapd05@gmail.com>
2024-10-12 13:16:25 -0400

    Partidos con data traida desde el back

--- frontend/src/users/components/MatchCard.jsx
+++ frontend/src/users/components/MatchCard.jsx
@@ -11,30 +11,33 @@ const MatchCard = ({
   awayTeam,
   fixtureDate,
   displayData,
+  status,
 }) => {
   const date = new Date(fixtureDate);
   const formattedTime = format(date, "HH:mm");
+  const isFinished = status.short === "FT";
+
   return (
-    <Card className="w-full max-w-sm overflow-hidden rounded-xl bg-[#F3F4F5] shadow-lg my-2">
+    <Card className="matchCard w-full max-w-sm overflow-hidden rounded-xl bg-[#F3F4F5] shadow-lg my-2">
       <div className="bg-white p-2 flex items-center space-x-2 pl-4">
-        <img src={leagueLogo} alt={leagueName} className="w-6 h-6" />
+        <img src={leagueLogo} alt={leagueName} className="w-6 object-cover" />
         <span className="text-gray-700 font-medium">{leagueName}</span>
       </div>
       <div className="p-4 mt-2">
         <div className="flex items-center justify-between mb-4">
-          <div className="flex flex-col items-center space-y-1 flex-1">
-            <img src={homeTeam.logo} alt={homeTeam.name} className="size-12" />
-            <span className="font-semibold text-center">{homeTeam.name}</span>
+          <div className="flex flex-col items-center space-y-2 flex-1">
+            <img src={homeTeam.logo} alt={homeTeam.name} className="size-10" />
+            <span className="font-normal text-center">{homeTeam.name}</span>
           </div>
           <div className="flex flex-col items-center justify-center flex-1">
-            {displayData.type === "result" ? (
-              <div className="flex flex-col items-center justify-center">
-                <span className="text-lg font-normal">Finalizado</span>
+            {isFinished ? (
+              <div className="flex flex-col items-center justify-center space-y-4">
+                <span className="text-sm font-normal">Finalizado</span>
                 <div className="score space-x-2 flex flex-row items-center">
                   <span className="font-semibold text-black text-2xl">
                     {displayData.homeTeamGoals}
                   </span>
-                  <Minus className="size-7" />
+                  <Minus className="size-4" />
                   <span className="font-semibold text-black text-2xl">
                     {displayData.awayTeamGoals}
                   </span>
@@ -43,18 +46,17 @@ const MatchCard = ({
             ) : (
               <div className="flex flex-col items-center justify-around space-y-2">
                 <span className="text-lg font-medium">{formattedTime}</span>
-                <span className="font-semibold">FT</span>
               </div>
             )}
           </div>
-          <div className="flex flex-col items-center space-y-1 flex-1">
-            <img src={awayTeam.logo} alt={awayTeam.name} className="size-12" />
-            <span className="font-semibold text-center">{awayTeam.name}</span>
+          <div className="flex flex-col items-center space-y-2 flex-1">
+            <img src={awayTeam.logo} alt={awayTeam.name} className="size-10" />
+            <span className="font-normal text-center">{awayTeam.name}</span>
           </div>
         </div>
         <div className="flex justify-around text-sm">
-          {displayData.type === "odds" &&
-            (displayData.value ? (
+          {!isFinished &&
+            (displayData.type === "odds" && displayData.value ? (
               <>
                 <Button className="bg-white hover:bg-gray-200 text-black font-normal text-xs px-5 py-1 leading-[18px]">
                   {displayData.value.home}
