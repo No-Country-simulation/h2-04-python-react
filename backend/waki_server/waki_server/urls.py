97e0d33a1a0ee969df7588fd5a6c6589d9ed28aa
Fernando <fer.gab.sua@gmail.com>
2024-10-23 16:07:40 -0300

    Update urls.py

--- backend/waki_server/waki_server/urls.py
+++ backend/waki_server/waki_server/urls.py
@@ -55,5 +55,5 @@ urlpatterns = [
     path('predictions/available/', predicciones_disponibles, name='predictions-available'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
