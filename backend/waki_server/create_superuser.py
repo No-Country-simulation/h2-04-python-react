383439d6ed5315c891946d4f7f0e06ab1bcea586
Fernando <fer.gab.sua@gmail.com>
2024-10-23 16:22:40 -0300

    debug true

--- backend/waki_server/create_superuser.py
+++ backend/waki_server/create_superuser.py
@@ -1,13 +1,24 @@
 import os
 import django
-from django.core.management import call_command
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'waki_server.settings')
+# Configura el entorno de Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "waki_server.settings")
 django.setup()
 
-# Cambia estos valores según tu configuración
-username = 'admin'
-email = 'admin@example.com'
-password = 'admin'
+from django.contrib.auth import get_user_model
 
-call_command('createsuperuser', interactive=False, username=username, email=email, password=password)
+def create_superuser():
+    User = get_user_model()
+    username = "admin"  # Cambia el nombre de usuario si lo deseas
+    email = "admin@example.com"  # Cambia la dirección de correo si lo deseas
+    password = "admin"  # Cambia la contraseña si lo deseas
+
+    if not User.objects.filter(username=username).exists():
+        user = User.objects.create_superuser(username=username, email=email, password=password)
+        user.save()  # Guarda el superusuario
+        print("Superuser created successfully.")
+    else:
+        print("Superuser already exists.")
+
+if __name__ == "__main__":
+    create_superuser()
