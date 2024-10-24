ab6b15f32743792ebe091b8d194de264fcdbe2c1
Fernando <fer.gab.sua@gmail.com>
2024-10-24 12:38:32 -0300

    conf media

--- backend/waki_server/waki_server/settings.py
+++ backend/waki_server/waki_server/settings.py
@@ -162,9 +162,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_ROOT = '/home/h2-04-python-react/backend/static/'
+# Esto es opcional pero recomendado para asegurarte de que los archivos estáticos sean servidos correctamente
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
